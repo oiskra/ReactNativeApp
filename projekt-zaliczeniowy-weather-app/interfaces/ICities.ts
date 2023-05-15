6f2a89b117d3ad6a238c0444f695f47bf04ded21
oiskra <ojaskra@gmail.com>
2023-05-15 18:37:04 +0200

    interface name change

---
+++ projekt-zaliczeniowy-weather-app/interfaces/ICities.ts
@@ -0,0 +1,19 @@
+export interface ICities {
+    error: boolean;
+    msg: string;
+    data: [
+        {
+            city: string;
+            country: string;
+            populationCounts: Array<
+                {
+                    year: string;
+                    value: string;
+                    sex: string;
+                    rehability: string;
+                }
+            >
+            
+        }
+    ]
+}
