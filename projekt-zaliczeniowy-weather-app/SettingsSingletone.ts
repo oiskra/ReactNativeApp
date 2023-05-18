4b6631f468a67940dae1b959513258a3cc619270
Marek Porębski <79985149+Marek572@users.noreply.github.com>
2023-05-18 16:28:38 +0200

    SettingsSingletone

---
+++ projekt-zaliczeniowy-weather-app/SettingsSingletone.ts
@@ -0,0 +1,41 @@
+type Theme = 'light' | 'dark';
+type Unit = 'celcius' | 'fahrenheit';
+
+class SettingsSingleton {
+
+    private static instance: SettingsSingleton;
+
+    private _theme: Theme = 'light';
+    private _unit: Unit = 'celcius';
+
+    private constructor() {}
+
+    public static getInstance(): SettingsSingleton {
+        if (!SettingsSingleton.instance) {
+            SettingsSingleton.instance = new SettingsSingleton();
+        }
+
+        return SettingsSingleton.instance;
+    }
+
+    public get theme(): string {
+        return this._theme;
+    }
+
+    public get unit(): string {
+        return this._unit;
+    }
+
+    public changeTheme() {
+
+        this._theme = this._theme === 'light' ? 'dark' : 'light';
+
+    }
+
+    public changeUnit() {
+
+        this._unit = this._unit === 'celcius' ? 'fahrenheit' : 'celcius';
+
+    }
+
+}
